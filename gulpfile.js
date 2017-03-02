08d13fcc90c55623bee71dca9730d3a12d93beb2
Naomi Mine <naomi.mine@gmail.com>
2017-03-02 07:10:41 -0600

    first commit

--- gulpfile.js
+++ gulpfile.js
@@ -5,13 +5,6 @@ var watch = require('gulp-watch');
 // watch task listen for saves on a specific file
 //then runs, the task you want
 
-gulp.task('watch', function(){
-  // first argument is an array of files
-  // second argument is an array of tasks to run
-  // * is telling gulp to watch every file
-  // in the folder with an extension of .less
-  gulp.watch(['./styles/*.less'], ['compillle-less'])
-})
 
 gulp.task('compile-less', function(){
   // ./ starts you at the same level as whatever
@@ -26,5 +19,12 @@ gulp.task('compile-less', function(){
 // default name, second arg is an array of tasks to be run
 gulp.task('default', ['compile-less', 'watch'])
 
+//lessc styles/style.less styles/style.css
 
-
+gulp.task('watch', function(){
+  // first argument is an array of files
+  // second argument is an array of tasks to run
+  // * is telling gulp to watch every file
+  // in the folder with an extension of .less
+  gulp.watch(['./styles/style.less'], ['compile-less'])
+})
--- server/app.js
+++ server/app.js
@@ -1,19 +1,38 @@
 var express = require('express'),
     app     = express(),
-    server  = require('http').createServer(app);
+    server  = require('http').createServer(app),
+	path = require('path');
 
+app.use(express.static(path.join(__dirname, 'public')));
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
 
+//handlebars
+app.get('/home', function(request, response){
+	response.render('home')
+})
+app.get('/vacay', function(request, response){
+	response.render('vacay', {name: "Naomi", loggedIn: true, perks: ["Spa day", "Unlimited compliments", "Smoked salmon"]})
+})
 
-  app.get('/', function(request, response){
+//Is this what you meant by message text? Or console.log()?
+app.get('/', function(request, response){
     // request object is from the client,
     // the response is what were sending back
-    response.send('Hey I the server finally works')
+response.send('Hey, you come here often?')
   })
 
-
+//JSON
+app.get('/bluebuddy', function(request, response){
+	  response.send({id: 1, type: "FruitFriend", name: "Boris", weeksOld: 15});
+  })
+//JSON
+app.get('/professor', function(request, response){
+	  response.send({id: 2, type: "FruitFriend", name: "Professor Peach", weeksOld: 20});
+  })
 
 // first argument is the port number
 server.listen(5000, function(){
-  console.log('server is listening, but on what port, are you reading the code?')
+  console.log('server is listening on port 5000')
 })
